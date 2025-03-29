9560b248bf4c9785c3fe1d89b32a2d7e56b4739b
Anton <np.dev@yandex.ru>
2025-03-29 20:36:37 +0300

    Добавлен макет экрана профиля.

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var userPickImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    // MARK: - Overrides Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Настройка элементов экрана.
+        userPickImageView.layer.cornerRadius = userPickImageView.bounds.width / 2
+        userNameLabel.setCharacterSpacing(0.08)
+    }
+    
+    // MARK: - IB Actions
+    
+    @IBAction func logoutButtonTap(_ sender: Any) { }
+}
