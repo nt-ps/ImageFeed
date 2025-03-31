57f88d6fb8fdc99963ceb2dc22fee8e998036e8e
Anton <np.dev@yandex.ru>
2025-03-31 21:42:31 +0300

    Добавлен экран просмотра изображения.

---
+++ ImageFeed/ImageViewer/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - Overrides Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
