6a581c1464e66a6ae104560f7b8a0a83ab4d3696
Anton <np.dev@yandex.ru>
2025-04-19 13:57:27 +0300

    Добавлен ProgressView.

--- ImageFeed/Authentication/WebViewViewController.swift
+++ ImageFeed/Authentication/WebViewViewController.swift
@@ -6,6 +6,7 @@ final class WebViewViewController: UIViewController {
     // MARK: - Views
     
     private var webView: WKWebView?
+    private var progressView: UIProgressView?
     
     // MARK: - Public Properties
     
@@ -17,13 +18,49 @@ final class WebViewViewController: UIViewController {
         super.viewDidLoad()
         
         addWebView()
-        loadAuthView()
+        addProgressView()
         
         webView?.navigationDelegate = self
+        
+        loadAuthView()
+        updateProgress()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        webView?.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+        updateProgress()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        webView?.removeObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            context: nil)
+    }
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
     }
     
     // MARK: - Private Methods
-
+    
     private func addWebView() {
         let webView = WKWebView()
         
@@ -41,6 +78,23 @@ final class WebViewViewController: UIViewController {
         self.webView = webView
     }
     
+    private func addProgressView() {
+        let progressView = UIProgressView()
+        
+        progressView.progressTintColor = .ypBlack
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(progressView)
+        
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        self.progressView = progressView
+    }
+    
     private func loadAuthView() {
         guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { return }
         
@@ -56,6 +110,16 @@ final class WebViewViewController: UIViewController {
         let request = URLRequest(url: url)
         webView?.load(request)
     }
+    
+    private func updateProgress() {
+        guard
+            let progressView = self.progressView,
+            let webView = self.webView
+        else { return }
+        
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
 }
 
 extension WebViewViewController: WKNavigationDelegate {
@@ -65,7 +129,7 @@ extension WebViewViewController: WKNavigationDelegate {
         decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
     ) {
          if let code = code(from: navigationAction) {
-             //TODO: process code
+             delegate?.webViewViewController(self, didAuthenticateWithCode: code)
              decisionHandler(.cancel)
          } else {
              decisionHandler(.allow)
